8625a74a5cb29b54279c2194896e379e99777892
pixelplex <dev@pixelplex.io>
2019-10-28 08:46:04 +0000

    Added compiler version v0.5.1

--- bin/list.js
+++ bin/list.js
@@ -1,11 +1,13 @@
 
 var soljsonSources = [
+  "soljson-v0.5.1.js",
   "soljson-v0.5.0.js",
   "soljson-v0.4.26.js",
   "soljson-v0.4.25.js",
   "soljson-v0.4.24.js"
 ];
 var soljsonReleases = {
+  "0.5.1": "soljson-v0.5.1.js",
   "0.5.0": "soljson-v0.5.0.js",
   "0.4.26": "soljson-v0.4.26.js",
   "0.4.25": "soljson-v0.4.25.js",
